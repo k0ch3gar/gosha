639ef2603d40a721c5380ea7ebd83180b9d7725a
k0ch3gar <kstmc239@outlook.com>
2025-08-21 09:07:41 -0500

    feat: added second chan example with type conversion

--- examples/chan-example2.sh
+++ examples/chan-example2.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/gosha
 
-var a chan string
+var first = make(chan int, 1)
+var second = make(chan string, 1)
 
-func worker() {
-  var val = ""
-  for val != "q" {
-    val = <-a
+func workers() {
+  for true {
+    val := make(int, <- second)
     print(val)
+    if val == 0 {
+      break
+    }
+  }
+}
+
+func workerf() {
+  for true {
+    second<- make(string, <-first)
   }
 }
 
 func generator() {
-  var val = ""
-  for val != "q" {
+  for true {
+    var val int
     read(&val)
-    a <- val
+    first <- val
   }
 }
 
-go worker()
-generator()
+go workerf()
+go generator()
+workers()
